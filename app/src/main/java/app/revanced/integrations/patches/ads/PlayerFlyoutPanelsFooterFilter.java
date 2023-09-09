9ab2439f59d60c47b83c1d6e30469bfada683973
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2023-09-09 19:58:30 +0700

    fix: Not worked

--- app/src/main/java/app/revanced/integrations/patches/ads/PlayerFlyoutPanelsFooterFilter.java
+++ app/src/main/java/app/revanced/integrations/patches/ads/PlayerFlyoutPanelsFooterFilter.java
@@ -3,15 +3,10 @@ package app.revanced.integrations.patches.ads;
 import androidx.annotation.Nullable;
 
 import app.revanced.integrations.settings.SettingsEnum;
-import app.revanced.integrations.utils.StringTrieSearch;
 
 final class PlayerFlyoutPanelsFooterFilter extends Filter {
-    private final StringTrieSearch exceptions = new StringTrieSearch();
 
     public PlayerFlyoutPanelsFooterFilter() {
-        exceptions.addPatterns(
-            "bottom_sheet_list_option"
-        );
 
         pathFilterGroups.addAll(
             new StringFilterGroup(
@@ -31,7 +26,7 @@ final class PlayerFlyoutPanelsFooterFilter extends Filter {
     boolean isFiltered(String path, @Nullable String identifier, String allValue, byte[] protobufBufferArray,
                        FilterGroupList matchedList, FilterGroup matchedGroup, int matchedIndex) {
         // If the flyout is not caption or quality, skip
-        if (!path.contains("captions_sheet_content.eml-js") || !path.contains("advanced_quality_sheet_content.eml-js") || exceptions.matches(path))
+        if (!path.contains("captions_sheet_content.eml-js") || !path.contains("advanced_quality_sheet_content.eml-js") || path.contains("bottom_sheet_list_option"))
             return false;
 
         return super.isFiltered(path, identifier, allValue, protobufBufferArray, matchedList, matchedGroup, matchedIndex);
